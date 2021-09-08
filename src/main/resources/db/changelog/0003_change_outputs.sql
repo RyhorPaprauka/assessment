9cee33b6836ff2abed2418c7c69b999891803fd7
GrigoryP, Sogo(pm2081) <pm2081@intl.att.com>
2021-09-08 10:38:13 +0300

    [outputs] chanhe ids

---
+++ src/main/resources/db/changelog/0003_change_outputs.sql
@@ -0,0 +1,7 @@
+UPDATE node
+SET outputs = '[{"id":"port-2","alignment":"bottom"}]'
+WHERE node.title = 'HTML';
+
+UPDATE node
+SET outputs = '[{"id":"port-3","alignment":"bottom"}]'
+WHERE node.title = 'CSS';
